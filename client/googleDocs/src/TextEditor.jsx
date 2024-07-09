d6f78c07b509231a3df43c0b3d1fa598d2e6fc85
Rut Birhanu <birhanurut@gmail.com>
2024-07-09 18:20:21 +0300

    adding toolbar

--- client/googleDocs/src/TextEditor.jsx
+++ client/googleDocs/src/TextEditor.jsx
@@ -1,16 +1,24 @@
+import Quill from "quill"
 import "quill/dist/quill.snow.css"
 import { useCallback, } from "react"
-import Quill from "quill"
+
+const TOOLBAR_OPTIONS = [
+    [{ header: [1, 2, 3, 4, 5,6,false] }],
+    [{ list: "ordered" }, { list: "bullet" }],
+    ["bold", "italic", "underline"],
+    ["image", "blockquote"],
+    ["clean"]
+]
 
 function TextEditor() {
 
     const wrapperRef = useCallback((wrapper) => {
-        
-        if (wrapper==null) return 
+
+        if (wrapper == null) return
         wrapper.innerHTML = ""
         const editor = document.createElement("div")
         wrapper.append(editor)
-        new Quill(editor, { theme: "snow" })
+        new Quill(editor, { theme: "snow", modules: {toolbar:TOOLBAR_OPTIONS} })
     },
         [])
     return (
